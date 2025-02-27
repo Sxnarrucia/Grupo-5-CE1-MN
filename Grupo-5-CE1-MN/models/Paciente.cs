0015c2ef624c922310e71226d39b90dabe63d0ed
Josue Calderon <ronaldjosuecb10@hotmail.com>
2025-02-26 19:00:32 -0600

    Clase Paciente

---
+++ Grupo-5-CE1-MN/models/Paciente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Grupo_5_CE1_MN.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del paciente es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Los apellidos del paciente son obligatorios")]
+        [StringLength(150)]
+        public string Apellidos { get; set; }
+
+        [Phone(ErrorMessage = "El número de teléfono no es válido")]
+        public string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "El correo no tiene un formato válido")]
+        public string Correo { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [StringLength(250)]
+        public string Direccion { get; set; }
+    }
+
+
+
+
+
+}
