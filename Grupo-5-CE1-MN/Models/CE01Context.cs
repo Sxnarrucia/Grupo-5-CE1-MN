60e1c211398352b1eda807460b80c80a3a6e81b6
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2025-02-26 19:56:57 -0600

    Context Added, Connection String added

---
+++ Grupo-5-CE1-MN/Models/CE01Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Grupo_5_CE1_MN.Models
+{
+    public class CE01Context : DbContext
+    {
+        public CE01Context() : base("name=CE01") 
+        { }
+
+        public DbSet<CitaMedica> CitaMedica { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<HistorialMedico> HistorialMedico { get; set; }
+        public DbSet<Paciente> Paciente { get; set; }
+
+    }
+}
